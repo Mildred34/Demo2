59e5527ef09896019f775ca8ea5a051ad9479a66
Mildred34 <alexis-h-34@hotmail.fr>
2018-10-16 15:41:05 +0200

    Ajout de commentaires

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void main(){
 	Zfraction a(4,5),b(2),c,d;      //Déclare une fraction valant 4/5
-	c = a + b;
+	c = a + b; // Operation d'addition entre 2 fractions utilisant la surchage d'opérateur +
 	c.afficher();
 	cout << c;
 }
